07b6f221578b50e939a2c01698ba869d3b9dc4c0
Rajendra singh <rawatrajendrasingh33185@gmail.com>
2026-03-17 11:15:00 +0530

    Delete prince directory

--- prince/main.js
+++
@@ -1,8 +0,0 @@
-function loadScript(src){
-let s=document.createElement("script");
-s.src=src;
-document.body.appendChild(s);
-}
-
-loadScript("js/timer.js");
-loadScript("js/ui.js");
--- prince/ui.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("contextmenu", e => e.preventDefault());
-
-document.onkeydown = function(e){
-if(e.keyCode==123) return false;
-if(e.ctrlKey && e.shiftKey && e.keyCode==73) return false;
-};
